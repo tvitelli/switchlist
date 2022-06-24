cde62b3357a148f5800bad6ecf5bd16533076633
tvitelli <tvitelli@comcast.net>
2022-06-24 16:12:21 -0500

    added regex validation for Reporting Mark

---
+++ src/main/resources/templates/regexReportingMark.js
@@ -0,0 +1,16 @@
+
+let reportingMark = document.getElementById('reportingMark');
+let equipment = document.getElementById('equipment');
+let reportingMarkErr = document.getElementById('reportingMarkErr');
+
+reportingMark.addEventListener('input', function(e) {
+    let pattern = /(^([A-Z]{2,4})([0-9]{1,6})$)/;
+    let currentValue = e.target.value;
+    let valid = pattern.test(currentValue);
+
+    if (valid) {
+        reportingMarkErr.style.display = 'none'
+    } else {
+        reportingMarkErr.placeholder = "Enter Reporting Mark in format AAAA000000"
+    }
+})
